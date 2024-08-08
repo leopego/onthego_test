c6dbb338405a1c5c586138984ff8f3eee061434f
leonardo <leonardopegomartins@gmail.com>
2024-08-08 00:31:01 -0300

    feat: add icons

---
+++ app/assets/icons/plus.tsx
@@ -0,0 +1,20 @@
+import { SvgIcon } from '@mui/material';
+
+export function PlusIcon() {
+  return (
+    <SvgIcon sx={{ fontSize: 16 }}>
+      <svg
+        width='16'
+        height='16'
+        viewBox='0 0 16 16'
+        fill='none'
+        xmlns='http://www.w3.org/2000/svg'
+      >
+        <path
+          d='M9.14286 16V9.14286H16V6.85714L9.14286 6.85714L9.14286 0L6.85714 0L6.85714 6.85714L0 6.85714L0 9.14286L6.85714 9.14286L6.85714 16H9.14286Z'
+          fill='white'
+        />
+      </svg>
+    </SvgIcon>
+  );
+}
